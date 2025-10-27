97368ed21c45675e8e06a43f1cf5637412a70b2d
nuno maduro <enunomaduro@gmail.com>
2025-10-27 10:48:32 +0000

    chore: prevents process stray requests

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,16 +2,20 @@
 
 declare(strict_types=1);
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Sleep;
 use Illuminate\Support\Str;
+use Tests\TestCase;
 
-pest()->extend(Tests\TestCase::class)
-    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+pest()->extend(TestCase::class)
+    ->use(RefreshDatabase::class)
     ->beforeEach(function (): void {
         Str::createRandomStringsNormally();
         Str::createUuidsNormally();
         Http::preventStrayRequests();
+        Process::preventStrayProcesses();
         Sleep::fake();
 
         $this->freezeTime();
@@ -20,4 +24,7 @@ pest()->extend(Tests\TestCase::class)
 
 expect()->extend('toBeOne', fn () => $this->toBe(1));
 
-function something(): void {}
+function something(): void
+{
+    // ..
+}
