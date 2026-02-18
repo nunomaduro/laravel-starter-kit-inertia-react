8b57eaa2e0e8be33bb097ad8b937247609440cf0
Nuno Maduro <enunomaduro@gmail.com>
2026-02-18 13:58:37 +0000

    chore: standardize comment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 final class DatabaseSeeder extends Seeder
