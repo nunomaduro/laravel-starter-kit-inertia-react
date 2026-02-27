786b951ae401e70cfb50e81d1f9e48212b692f2a
Hardik Shah <sunshine.mahant@gmail.com>
2026-02-27 16:34:57 +1100

    Update app-header.tsx

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -70,7 +70,7 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
     const getInitials = useInitials();
     return (
         <>
-            <div className="border-b border-sidebar-border/80 bg-muted">
+            <div className="border-b border-sidebar-border/80 bg-card">
                 <div className="mx-auto flex h-16 items-center px-4 md:max-w-7xl">
                     {/* Mobile Menu */}
                     <div className="lg:hidden">
