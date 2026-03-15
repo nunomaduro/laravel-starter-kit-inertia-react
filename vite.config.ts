cc73fafba80628d027571a6aa96dc2fc59b04c0f
Josh Donnell <joshua.donnell1@gmail.com>
2026-03-15 11:59:08 +0000

    fix: lint and fix bun issue with vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,60 +5,60 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite-plus';
 
 export default defineConfig({
-  lint: {
-      options: {
-          typeAware:true,
-          typeCheck:true
-      },
-      plugins: ['eslint', 'typescript', 'unicorn', 'oxc', 'react']
-  },
-  fmt: {
-      printWidth: 80,
-      tabWidth: 4,
-      useTabs: false,
-      semi: true,
-      singleQuote: true,
-      overrides: [
-          {
-              files: ['**/*.yml'],
-              options: {
-                  'tabWidth': 2
-              }
-          }
-      ],
-      sortTailwindcss: {
-          functions: ['clsx', 'cn'],
-          stylesheet: 'resources/css/app.css'
-      },
-      sortImports: {
-          groups: [
-              'builtin',
-              'external',
-              'internal',
-              'parent',
-              'sibling',
-              'index'
-          ],
-          newlinesBetween: false
-      },
-      ignorePatterns: [
-          'resources/js/components/ui/*',
-          'resources/views/mail/*',
-          'resources/js/actions/*',
-          'resources/js/routes/*',
-          'resources/js/wayfinder/*'
-      ],
-  },
-  plugins: [
-      laravel({
-          input: ['resources/css/app.css', 'resources/js/app.tsx'],
-          ssr: 'resources/js/ssr.tsx',
-          refresh: true,
-      }),
-      react(),
-      tailwindcss(),
-      wayfinder({
-          formVariants: true,
-      }),
-  ],
+    lint: {
+        options: {
+            typeAware: true,
+            typeCheck: true,
+        },
+        plugins: ['eslint', 'typescript', 'unicorn', 'oxc', 'react'],
+    },
+    fmt: {
+        printWidth: 80,
+        tabWidth: 4,
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+        overrides: [
+            {
+                files: ['**/*.yml'],
+                options: {
+                    tabWidth: 2,
+                },
+            },
+        ],
+        sortTailwindcss: {
+            functions: ['clsx', 'cn'],
+            stylesheet: 'resources/css/app.css',
+        },
+        sortImports: {
+            groups: [
+                'builtin',
+                'external',
+                'internal',
+                'parent',
+                'sibling',
+                'index',
+            ],
+            newlinesBetween: false,
+        },
+        ignorePatterns: [
+            'resources/js/components/ui/*',
+            'resources/views/mail/*',
+            'resources/js/actions/*',
+            'resources/js/routes/*',
+            'resources/js/wayfinder/*',
+        ],
+    },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            ssr: 'resources/js/ssr.tsx',
+            refresh: true,
+        }),
+        react(),
+        tailwindcss(),
+        wayfinder({
+            formVariants: true,
+        }),
+    ],
 });
