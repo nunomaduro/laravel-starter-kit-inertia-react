e7e486f35b12b45acdbb5a3d9576e221cd328001
Carlos Mayorga <cmayorgahilario@gmail.com>
2026-03-30 01:49:32 -0500

    Refactor Blade template to use `<x-inertia::head>` component for Inertia head rendering.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,8 +30,6 @@
             }
         </style>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
@@ -41,7 +39,9 @@
 
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
-        <x-inertia-head />
+        <x-inertia::head>
+            <title>{{ config('app.name', 'Laravel') }}</title>
+        </x-inertia::head>
     </head>
     <body class="font-sans antialiased">
         <x-inertia />
