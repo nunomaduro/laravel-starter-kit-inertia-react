cf9c0d2a13600aa700188b1b8da4846ce6e09e9b
Josh Donnell <joshua.donnell1@gmail.com>
2026-04-29 08:12:18 +0100

    feat: add toasts from laravel starter kit

--- resources/js/pages/user-password/edit.tsx
+++ resources/js/pages/user-password/edit.tsx
@@ -1,4 +1,3 @@
-import { Transition } from '@headlessui/react';
 import { Form, Head } from '@inertiajs/react';
 import { useRef } from 'react';
 import UserPasswordController from '@/actions/App/Http/Controllers/UserPasswordController';
@@ -57,7 +56,7 @@ export default function Password() {
                         }}
                         className="space-y-6"
                     >
-                        {({ errors, processing, recentlySuccessful }) => (
+                        {({ errors, processing }) => (
                             <>
                                 <div className="grid gap-2">
                                     <Label htmlFor="current_password">
@@ -120,18 +119,6 @@ export default function Password() {
                                     >
                                         Save password
                                     </Button>
-
-                                    <Transition
-                                        show={recentlySuccessful}
-                                        enter="transition ease-in-out"
-                                        enterFrom="opacity-0"
-                                        leave="transition ease-in-out"
-                                        leaveTo="opacity-0"
-                                    >
-                                        <p className="text-sm text-neutral-600">
-                                            Saved
-                                        </p>
-                                    </Transition>
                                 </div>
                             </>
                         )}
